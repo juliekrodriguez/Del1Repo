b230ae8236c1ddd9a4d1655eb658fcf84742495b
julie <julie.rodriguez1@wsu.edu>
2022-11-13 20:53:22 -0800

    more test

---
+++ fixedDel1/src/MScheckstyle/J2Check.java
@@ -0,0 +1,74 @@
+package MScheckstyle;
+import java.util.Random; 
+
+public class J2Check {
+    public static void main(String[] args)
+    {
+        char chartoint = 6;
+        int testint = (int) chartoint;
+        int testint1 = (int) chartoint;
+        int testint2 = (int) chartoint;
+        long testint3 = (long) chartoint;
+        double testint4 = (double) chartoint;
+        int whileCount = 0;
+        int size = 10;
+        Random rand = new Random();
+        int randm = rand.nextInt(8);
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                randm = rand.nextInt(8);
+                if(randm == 0)
+                {
+                    System.out.print(" Z ");
+                }
+                if(randm == 1)
+                {
+                    System.out.print(" A ");
+                }
+                if(randm == 2)
+                {
+                    System.out.print(" B ");
+                }
+                if(randm == 3)
+                {
+                    System.out.print(" C ");
+                }
+                if(randm == 4)
+                {
+                    System.out.print(" D ");
+                }
+                if(randm == 5)
+                {
+                    System.out.print(" E ");
+                }
+                if(randm == 6)
+                {
+                    System.out.print(" F ");
+                }
+                if(randm == 7)
+                {
+                    System.out.print(" G ");
+                }
+                if(randm == 8)
+                {
+                    System.out.print(" H ");
+                }
+            }
+            System.out.println("");
+        }
+        while (whileCount < 10)
+        {
+            System.out.print(whileCount);
+            whileCount++;
+            for (int i = 0; i < 10; i++)
+            {
+                
+            }
+        }
+
+
+    }
+
+}
---
+++ fixedDel1/src/MScheckstyle/LoopingCheckUnitTest.java
@@ -0,0 +1,70 @@
+package MScheckstyle;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Assert;
+
+import org.junit.jupiter.api.Test;
+
+import com.puppycrawl.tools.checkstyle.DefaultConfiguration;
+import com.puppycrawl.tools.checkstyle.DefaultContext;
+import com.puppycrawl.tools.checkstyle.JavaParser;
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.FileContents;
+import com.puppycrawl.tools.checkstyle.api.FileText;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+public class LoopingCheckUnitTest {
+    public loopingCheck unit = new loopingCheck();
+    public int[] unitC =  new int[] { TokenTypes.LITERAL_FOR, TokenTypes.LITERAL_WHILE };
+    @Test
+    public void testgetAcceptableTokens() {
+        Assert.assertArrayEquals(unitC,unit.getAcceptableTokens());
+    }
+    @Test
+    public void testgetDefaultTokens() {
+        Assert.assertArrayEquals(unitC,unit.getRequiredTokens());
+    }
+    @Test
+    public void testgetRequiredTokens() {
+        int[] unitC = new int[0];
+        Assert.assertArrayEquals(unitC,unit.getRequiredTokens());
+    }
+    @Test
+    public void testBlackBoxLoop() throws IOException, CheckstyleException
+    {
+        loopingCheck check;
+        check = firstCheckB();
+        int count = check.getLoopCount();
+        assertTrue(count == 4);
+    }
+    private void help(AbstractCheck check, DetailAST ast)
+    {
+        while(ast!= null)
+        {
+            check.visitToken(ast);
+            help(check, ast.getFirstChild());
+            ast = ast.getNextSibling();
+        }
+    }
+    private loopingCheck firstCheckB() throws IOException, CheckstyleException
+    {
+        String fileP = System.getProperty("user.dir");
+        File file = new File(fileP + "/src/ MScheckstyle/J2Check.java");
+        FileText fileTxt = new FileText(file, "UTF-8");
+        FileContents fileCont = new FileContents(fileTxt);
+        DetailAST root = JavaParser.parse(fileCont);
+        loopingCheck check = new loopingCheck();
+        check.configure(new DefaultConfiguration("Local"));
+        check.contextualize(new DefaultContext());
+        check.beginTree(root);
+        help(check, root);
+        check.finishTree(root);
+        return check;
+    }
+    
+}
